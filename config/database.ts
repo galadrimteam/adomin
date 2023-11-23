5e4f6330be20a4ad08c2b62e8d5c3f5d4ea49f98
LE MONIES DE SAGAZAN Mayeul <m.lemoniesdesagazan@gmail.com>
2023-11-23 17:26:06 +0100

    chore: setup mysql / postgres

--- config/database.ts
+++ config/database.ts
@@ -5,8 +5,8 @@
  * file.
  */
 
-import Env from '@ioc:Adonis/Core/Env'
 import Application from '@ioc:Adonis/Core/Application'
+import Env from '@ioc:Adonis/Core/Env'
 import type { DatabaseConfig } from '@ioc:Adonis/Lucid/Database'
 
 const databaseConfig: DatabaseConfig = {
@@ -48,6 +48,60 @@ const databaseConfig: DatabaseConfig = {
         naturalSort: true,
       },
       useNullAsDefault: true,
+      healthCheck: false,
+      debug: false,
+    },
+
+    /*
+    |--------------------------------------------------------------------------
+    | MySQL config
+    |--------------------------------------------------------------------------
+    |
+    | Configuration for MySQL database. Make sure to install the driver
+    | from npm when using this connection
+    |
+    | npm i mysql2
+    |
+    */
+    mysql: {
+      client: 'mysql2',
+      connection: {
+        host: Env.get('MYSQL_HOST'),
+        port: Env.get('MYSQL_PORT'),
+        user: Env.get('MYSQL_USER'),
+        password: Env.get('MYSQL_PASSWORD', ''),
+        database: Env.get('MYSQL_DB_NAME'),
+      },
+      migrations: {
+        naturalSort: true,
+      },
+      healthCheck: false,
+      debug: false,
+    },
+
+    /*
+    |--------------------------------------------------------------------------
+    | PostgreSQL config
+    |--------------------------------------------------------------------------
+    |
+    | Configuration for PostgreSQL database. Make sure to install the driver
+    | from npm when using this connection
+    |
+    | npm i pg
+    |
+    */
+    pg: {
+      client: 'pg',
+      connection: {
+        host: Env.get('PG_HOST'),
+        port: Env.get('PG_PORT'),
+        user: Env.get('PG_USER'),
+        password: Env.get('PG_PASSWORD', ''),
+        database: Env.get('PG_DB_NAME'),
+      },
+      migrations: {
+        naturalSort: true,
+      },
       healthCheck: false,
       debug: false,
     },
