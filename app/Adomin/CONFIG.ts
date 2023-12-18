1f4c081d26683429e9b01891387b46a8b89f6849
LE MONIES DE SAGAZAN Mayeul <m.lemoniesdesagazan@gmail.com>
2023-12-18 19:22:46 +0100

    feat: v3.2.3

--- app/Adomin/CONFIG.ts
+++ app/Adomin/CONFIG.ts
@@ -1,8 +1,7 @@
-import { PROFILE_CONFIG, TEST_CONFIG, USER_CONFIG } from 'App/TestAdominConfig'
 import { AdominConfig } from './createModelConfig'
 
 export const ADOMIN_CONFIG: AdominConfig = {
   title: 'Adomin',
   userDisplayKey: 'email',
-  models: [USER_CONFIG, PROFILE_CONFIG, TEST_CONFIG],
+  models: [],
 }
