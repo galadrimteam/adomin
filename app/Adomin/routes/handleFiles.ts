a0d22296168679d39596603b5e010dec5cf5b9ba
LE MONIES DE SAGAZAN Mayeul <m.lemoniesdesagazan@gmail.com>
2023-12-21 19:29:14 +0100

    fix: type

--- app/Adomin/routes/handleFiles.ts
+++ app/Adomin/routes/handleFiles.ts
@@ -21,7 +21,7 @@ export const loadFilesForInstances = async (fields: ColumnConfig[], modelInstanc
 
   const promises = modelInstances.flatMap(async (modelInstance) => {
     const innerPromises = filesColumn.map(async ({ name }) => {
-      const attachment = modelInstance[name] as AttachmentContract | undefined
+      const attachment: AttachmentContract | undefined = modelInstance[name]
       if (!attachment || typeof attachment.url === 'string') return
       const url = await attachment.getUrl()
       attachment.url = url
