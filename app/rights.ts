d4b1da54eeb1f6421d886b89d8f970502912f468
LE MONIES DE SAGAZAN Mayeul <m.lemoniesdesagazan@gmail.com>
2024-02-16 16:02:07 +0100

    refactor: rights.ts

--- app/rights.ts
+++ app/rights.ts
@@ -1,8 +1,8 @@
 export const RIGHTS = {
   DEFAULT: 0b0,
   SUPER_ADMIN: 0b1,
-  CPI_GLOBAL: 0b10,
-  CHECKPOINT: 0b100,
+  ROLE_SYMPA: 0b10,
+  ROLE_HORRIBLE: 0b100,
   // Other rights...
 } as const
 
@@ -10,9 +10,9 @@ export type Right = keyof typeof RIGHTS
 
 export const RIGHTS_LABELS: { [K in Right]: string } = {
   DEFAULT: 'Sans roles',
-  CPI_GLOBAL: 'CPI Global',
+  ROLE_SYMPA: 'Role Sympa',
   SUPER_ADMIN: 'Super Admin',
-  CHECKPOINT: 'Checkpoint',
+  ROLE_HORRIBLE: 'Role Horrible',
 }
 
 export const RIGHTS_KEYS = Object.keys(RIGHTS) as Right[]
