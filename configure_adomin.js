78f6075635eba4b988450219e0135b10e61d6bc1
LE MONIES DE SAGAZAN Mayeul <m.lemoniesdesagazan@gmail.com>
2024-05-07 16:54:10 +0200

    feat: v6.1.2

--- configure_adomin.js
+++ configure_adomin.js
@@ -6,11 +6,12 @@ const tmpDir = fs.mkdtempSync('adomin-clone-')
 
 const COMMAND = `
 cd ${tmpDir}
-git clone -n --depth=1 --filter=tree:0 https://github.com/galadrimteam/adomin.git adomin-sparse
+git clone -n --depth=1 --filter=tree:0 https://github.com/galadrimteam/adomin.git adomin-sparse > /dev/null
 cd adomin-sparse
-git sparse-checkout set --no-cone app/adomin
-git checkout
+git sparse-checkout set --no-cone app/adomin > /dev/null
+git checkout > /dev/null
 cd ../..
+mkdir -p app
 cp -r ${tmpDir}/adomin-sparse/app/adomin app/adomin
 rm -rf ${tmpDir}`
 
